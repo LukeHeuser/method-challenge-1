0428bdfef767945ebae533f317201f30396f9dac
Luke Heuser <103976088+LukeHeuser@users.noreply.github.com>
2024-07-29 22:39:54 -0400

    First Comit

---
+++ src/Main.java
@@ -0,0 +1,41 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int highScorePosition = calculateHighScorePosition(1000);
+        displayHighScorePosition("Luke",highScorePosition);
+
+        highScorePosition = calculateHighScorePosition(100);
+        displayHighScorePosition("Hannah", highScorePosition);
+
+        highScorePosition = calculateHighScorePosition(25);
+        displayHighScorePosition("Rocco", highScorePosition);
+
+        highScorePosition = calculateHighScorePosition(500);
+        displayHighScorePosition("Chevy", highScorePosition);
+
+        highScorePosition = calculateHighScorePosition(1500);
+        displayHighScorePosition("Preston", highScorePosition);
+    }
+
+    public static void displayHighScorePosition(String playerName, int highScorePositionPosition) {
+
+        System.out.println(playerName + " managed to get into position " + highScorePositionPosition + " on" +
+                " the high score list!");
+    }
+
+    public static int calculateHighScorePosition(int playersScore) {
+
+        int position = 4;
+
+        if (playersScore >= 1000) {
+            position = 1;
+        } else if (playersScore >= 500) {
+            position = 2;
+        } else if (playersScore >= 100) {
+            position = 3;
+        }
+
+        return position;
+
+    }
+}
